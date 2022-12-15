4c5d5b73565ebc87158c4ee3b59ad6d42e516a8a
Jun Meng <chanjm1205@gmail.com>
2022-12-15 14:16:57 +0800

    Connect DB

--- app.js
+++ app.js
@@ -1,54 +1,74 @@
 //* A SIMPLE DAILY JOURNAL
 //TODO: Get familiar with app {.get .post .listen }
 //* .render("xxx") xxx points to xxx.ejs file
+//TODO brew services start mongodb-community@6.0
 
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const _ = require("lodash")
+const mongoose = require("mongoose")
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+mongoose.connect("mongodb://localhost:27017/blogDB")
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
-let posts = []
 
+const postSchema = {
+  title: String,
+  message: String,
+}
 
-app.get("/",(req,res)=>{
-  res.render("home",{homeStartingContent:homeStartingContent,posts:posts})
+const Post = mongoose.model("Post", postSchema)
+
+app.get("/", (req, res) => {
+  Post.find({},(err, posts) => {
+    res.render("home", {homeStartingContent:homeStartingContent, posts: posts })
+  })
 })
-app.get("/about",(req,res)=>{
-  res.render("about",{aboutContent:aboutContent})
+app.get("/about", (req, res) => {
+  res.render("about", { aboutContent: aboutContent })
 })
-app.get("/contact",(req,res)=>{
-  res.render("contact",{contactContent:contactContent})
+app.get("/contact", (req, res) => {
+  res.render("contact", { contactContent: contactContent })
 })
-app.get("/compose",(req,res)=>{
+app.get("/compose", (req, res) => {
   res.render("compose")
 })
-app.get("/post/:postId",(req,res)=>{
-  let requestedTitle = _.lowerCase(req.params.postId)
-  const matchTitle =()=>{
-    posts.find(post=>{
-      let postTitle = _.lowerCase(post.title)
-      if (postTitle === requestedTitle){
-        res.render("post",{post:post})
-      }
+app.get("/post/:postId", (req, res) => {
+  const requestedTitle = req.params.postId
+  // let requestedTitle = _.lowerCase(req.params.postId)
+  // const matchTitle = () => {
+  //   posts.find(post => {
+  //     let postTitle = _.lowerCase(post.title)
+  //     if (postTitle === requestedTitle) {
+  //       res.render("post", { post: post })
+  //     }
+  //   })
+  // }
+  // matchTitle()
+  Post.findOne({_id: requestedTitle}, (err, post)=>{
+    res.render("post",{
+      title: post.title,
+      message: post.message,
     })
-  } 
-  matchTitle()
+  })
 })
-app.post("/compose",(req,res)=>{
-  const post = {
-    title : req.body.titleMessage,
-    message : req.body.postMessage,
-  };
-  posts.push(post)
-  res.redirect("/")
+app.post("/compose", (req, res) => {
+  const post = new Post({
+    title: req.body.titleMessage,
+    message: req.body.postMessage,
+  });
+  //* Save to db instead of local
+  // posts.push(post)
+  post.save(err => {
+    if (!err) res.redirect("/")
+  })
 })
 
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
